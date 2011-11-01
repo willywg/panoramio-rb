35f98661669d2e398f9e4d7b9084d3ad17b66f83
WillyWG <willywg@gmail.com>
2011-10-31 21:27:51 -0500

    Get data from Panoramio API

--- lib/panoramio-rb.rb
+++ lib/panoramio-rb.rb
@@ -1,2 +1,27 @@
+require 'rubygems'
+require 'json'
+require 'hashie'
+require 'rest_client'
+
 module PanoramioRb
+  URL = 'http://www.panoramio.com/map/get_panoramas.php'
+  DEFAULT_OPTIONS = {
+    :set => :public,  # Cant be :public, :full, or a USER ID number
+    :size => :medium, # Cant be :original, :medium (default value), :small, :thumbnail, :square, :mini_square
+    :from => 0,
+    :to => 20,
+    :mapfilter => true
+  }
+  
+  def self.request_data(options = {})
+    panoramio_options = DEFAULT_OPTIONS
+    panoramio_options.merge!(options)
+    response = RestClient.get URL, :params => panoramio_options
+    if response.code == 200
+      parse_data = JSON.parse(response.to_str)
+      Hashie::Mash.new(parse_data)
+    else
+      raise "Panoramio API error: #{response.code}. Response #{response.to_str}"
+    end
+  end
 end
