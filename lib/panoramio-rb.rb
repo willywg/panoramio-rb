ca90c8b3929ee47b584b48995c439531b73970e3
William Wong Garay <willywg@gmail.com>
2016-05-26 19:44:39 -0500

    Restore hash style for ruby 1.8

--- lib/panoramio-rb.rb
+++ lib/panoramio-rb.rb
@@ -8,17 +8,17 @@ require 'geocoder/calculations'
 module PanoramioRb
   URL = 'http://www.panoramio.com/map/get_panoramas.php'
   DEFAULT_OPTIONS = {
-    set:        :public,  # Cant be :public, :full, or a USER ID number
-    size:       :medium, # Cant be :original, :medium (default value), :small, :thumbnail, :square, :mini_square
-    from:       0,
-    to:         20,
-    mapfilter:  true
+    :set => :public,  # Cant be :public, :full, or a USER ID number
+    :size => :medium, # Cant be :original, :medium (default value), :small, :thumbnail, :square, :mini_square
+    :from => 0,
+    :to => 20,
+    :mapfilter => true
   }
   
   def self.get_panoramas(options = {})
     panoramio_options = DEFAULT_OPTIONS
     panoramio_options = panoramio_options.merge(options)
-    response = RestClient.get URL, params: panoramio_options
+    response = RestClient.get URL, :params => panoramio_options
     if response.code == 200
       parse_data = JSON.parse(response.to_str)
       Hashie::Mash.new(parse_data)
@@ -28,12 +28,12 @@ module PanoramioRb
   end
 
   def self.get_panoramas_from_point(point, radius = 10, unit = :mi, options = {})
-    points = Geocoder::Calculations.bounding_box(point, radius, { units: unit })
+    points = Geocoder::Calculations.bounding_box(point, radius, { :units => unit })
     options.merge!({
-      miny: points[0],
-      minx: points[1],
-      maxy: points[2],
-      maxx: points[3] 
+      :miny => points[0],
+      :minx => points[1],
+      :maxy => points[2],
+      :maxx => points[3] 
     })
     self.get_panoramas(options)
   end
